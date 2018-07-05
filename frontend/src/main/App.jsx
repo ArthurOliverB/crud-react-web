e6638078852eb29faffdfc01643a56412ebe2d95
Arthur <arthuroliveirabandeira@gmail.com>
2018-07-05 00:31:13 -0300

    primeiro commit

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import { HashRouter } from 'react-router-dom'
+import Logo from '../components/templates/Logo'
+import Nav from '../components/templates/Nav'
+import Home from '../components/home/Home'
+import Routes from './Routes'
+import Footer from '../components/templates/Footer'
+
+export default props => 
+    
+    <HashRouter>
+        <div className="app">
+            <Logo />
+            <Nav />    
+            <Routes />
+            <Footer />
+        </div>
+    </HashRouter>
